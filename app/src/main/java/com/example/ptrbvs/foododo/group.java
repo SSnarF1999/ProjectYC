8667a1f36127515778947da29420125e14901bdf
joydroogsma <joydoerak@gmail.com>
2018-11-08 11:21:53 +0100

    alle klassen

--- app/src/main/java/com/example/ptrbvs/foododo/group.java
+++ app/src/main/java/com/example/ptrbvs/foododo/group.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class group {
 
     public static void groups(String[] args) {
-        username user01 = new username("hoi", "jekhkh", "18", "vega");
-        username user02 = new username("hoi01","89329", "17", "vegan");
-        username user03 = new username("hoi02", "kjfkjfs", "22", "nederlands");
+        username user01 = new username("hoi", "jekhkh", "18","","", "vega");
+        username user02 = new username("hoi01","89329", "17", "vegan", "","");
+        username user03 = new username("hoi02", "kjfkjfs", "22", "nederlands","","");
 
         ArrayList<username> username = new ArrayList<>();
         username.add(user01);
