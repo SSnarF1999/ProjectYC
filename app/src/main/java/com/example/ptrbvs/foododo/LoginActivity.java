d423740af656ded52ec6cbcdf76cb6c4ff1a5ff6
joydroogsma <joydoerak@gmail.com>
2018-11-13 12:13:23 +0100

    layout

--- app/src/main/java/com/example/ptrbvs/foododo/LoginActivity.java
+++ app/src/main/java/com/example/ptrbvs/foododo/LoginActivity.java
@@ -48,32 +48,15 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText etUsername = (EditText) findViewById(R.id.etUsename);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
-        final Button bLogin = (Button) findViewById(R.id.bLogin);
-        final TextView registerlink = (TextView) findViewById(R.id.tvRegister);
         final Button bPieter = (Button) findViewById(R.id.bPieter);
+        final Button bFrans = (Button) findViewById(R.id.bFrans);
+        final Button bTristan = (Button) findViewById(R.id.bTristan);
+        final Button bJoy = (Button) findViewById(R.id.bJoy);
         ImageButton ibPieter= (ImageButton) findViewById(R.id.ibPieter);
         ImageButton ibFrans= (ImageButton) findViewById(R.id.ibFrans);
         ImageButton ibTristan= (ImageButton) findViewById(R.id.ibTristan);
         ImageButton ibJoy= (ImageButton) findViewById(R.id.ibJoy);
-
-
-        registerlink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-                LoginActivity.this.startActivity(registerIntent);
-            }
-        });
-
-        bLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
-                LoginActivity.this.startActivity(loginIntent);
-            }
-        });
 
         bPieter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,16 +66,73 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        int imageRescource = getResources().getIdentifier("@drawable/images", null, this.getPackageName());
+        bFrans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                LoginActivity.this.startActivity(loginIntent);
+            }
+        });
+
+        bTristan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                LoginActivity.this.startActivity(loginIntent);
+            }
+        });
+
+        bJoy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                LoginActivity.this.startActivity(loginIntent);
+            }
+        });
+
+        ibPieter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                LoginActivity.this.startActivity(loginIntent);
+            }
+        });
+
+        ibFrans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                LoginActivity.this.startActivity(loginIntent);
+            }
+        });
+
+        ibTristan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                LoginActivity.this.startActivity(loginIntent);
+            }
+        });
+
+        ibJoy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                LoginActivity.this.startActivity(loginIntent);
+            }
+        });
+
+
+        int imageRescource = getResources().getIdentifier("@drawable/charmander", null, this.getPackageName());
         ibPieter.setImageResource(imageRescource);
 
-        int imageRescource1 = getResources().getIdentifier("@drawable/images", null, this.getPackageName());
+        int imageRescource1 = getResources().getIdentifier("@drawable/schildpad", null, this.getPackageName());
         ibFrans.setImageResource(imageRescource1);
 
         int imageRescource2 = getResources().getIdentifier("@drawable/images", null, this.getPackageName());
         ibTristan.setImageResource(imageRescource2);
 
-        int imageRescource3 = getResources().getIdentifier("@drawable/images", null, this.getPackageName());
+        int imageRescource3 = getResources().getIdentifier("@drawable/pokemon", null, this.getPackageName());
         ibJoy.setImageResource(imageRescource3);
     }
 
