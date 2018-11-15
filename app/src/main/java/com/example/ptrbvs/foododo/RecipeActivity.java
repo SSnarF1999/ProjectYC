9dca5bed0a48590c7a9245dc7619f0edc7450d3b
joydroogsma <joydoerak@gmail.com>
2018-11-15 14:49:47 +0100

    no message

--- app/src/main/java/com/example/ptrbvs/foododo/RecipeActivity.java
+++ app/src/main/java/com/example/ptrbvs/foododo/RecipeActivity.java
@@ -3,26 +3,57 @@ package com.example.ptrbvs.foododo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import java.util.ArrayList;
+
 public class RecipeActivity extends AppCompatActivity {
+    private ArrayList<ExampleItem> mExampleList;
+
+    private RecyclerView mRecyclerView;
+    private ExampleAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
 
-        final ImageButton bRecipe = (ImageButton) findViewById(R.id.bRecipe01);
+        createExampleList();
+        buildRecyclerView();
+    }
 
-        bRecipe.setOnClickListener(new View.OnClickListener() {
+    public void createExampleList() {
+        mExampleList = new ArrayList<>();
+        mExampleList.add(new ExampleItem(R.drawable.pokemon, "Pasta met rode saus","Pastapennen, tomaten, tomatensaus, ui, gehakt",""));
+        mExampleList.add(new ExampleItem(R.drawable.pokemon, "Zuurkool","1¼ kg kruimige aardappelen, 500 g zuurkool, 2 elstar-appels, 2 boeren scharrelrookworsten (à 250 g), 40 g ongezouten boter (of margarine), 150 ml halfvolle melk","Schil de aardappelen en kook in 20 min. gaar. Laat de zuurkool uitlekken. Voeg na 10 min. de zuurkool toe aan de aardappelen.Giet het mengsel af als de aardappelen gaar zijn. Vang wat kookvocht op.\n" +
+                "Schil de appels en snijd in kwarten. Verwijder het klokhuis en snijd in plakjes. Verwarm de worst. Verhit de boter en bak de appel 5 min. op hoog vuur. Verwarm de melk. Stamp de aardappelen en zuurkool grof. Voeg de melk toe en wat kookvocht. Schep de appel en boter erbij en serveer met de worst."));
+        mExampleList.add(new ExampleItem(R.drawable.pokemon, "Pizza Calzone","",""));
+        mExampleList.add(new ExampleItem(R.drawable.pokemon, "Brocolli-aardappeltaart","",""));
+    }
+
+    public void buildRecyclerView() {
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new ExampleAdapter(mExampleList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setOnItemClickListener(new ExampleAdapter.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent homeIntent = new Intent(RecipeActivity.this, RecipeIngredientsActivity.class);
-                RecipeActivity.this.startActivity(homeIntent);
+            public void onItemClick(int position) {
+                Intent intent = new Intent(RecipeActivity.this, RecipeIngredientsActivity.class);
+                intent.putExtra("Example Item", mExampleList.get(position));
+
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/ptrbvs/foododo/RecipeIngredientsActivity.java
+++ app/src/main/java/com/example/ptrbvs/foododo/RecipeIngredientsActivity.java
@@ -1,7 +1,10 @@
 package com.example.ptrbvs.foododo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RecipeIngredientsActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class RecipeIngredientsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_ingredients);
+
+
+        Intent intent = getIntent();
+        ExampleItem exampleItem = intent.getParcelableExtra("Example Item");
+
+        int imageRes = exampleItem.getImageResource();
+        String line1 = exampleItem.getText1();
+        String line2 = exampleItem.getText2();
+        String line3 = exampleItem.getText3();
+
+        ImageView imageView = findViewById(R.id.image_activity2);
+        imageView.setImageResource(imageRes);
+
+        TextView textView1 = findViewById(R.id.text1_activity2);
+        textView1.setText(line1);
+
+        TextView textView2 = findViewById(R.id.text2_activity2);
+        textView2.setText(line2);
+
+        TextView textView3 = findViewById(R.id.text2_activity3);
+        textView3.setText(line3);
     }
 }
